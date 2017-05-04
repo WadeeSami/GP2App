294f1196d1b0d1a9b10d4c2b608af1fc3226d192
Wadee Sami <wabuzant@endeavorpal.com>
2017-05-04 17:49:30 +0300

    add new activiy to write  a word

--- app/src/main/java/com/example/wadee/nots/MainActivity.java
+++ app/src/main/java/com/example/wadee/nots/MainActivity.java
@@ -1,73 +1,75 @@
 package com.example.wadee.nots;
 
 import android.app.Activity;
-import android.app.NotificationManager;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothSocket;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.os.Bundle;
-import android.support.v4.app.NotificationCompat;
+import android.os.Handler;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
-public class MainActivity extends Activity {
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.Set;
+import java.util.UUID;
 
+public class MainActivity extends Activity {
+    private String TAG = "GradPro";
     private TextView txtView;
     private NotificationReceiver nReceiver;
     private BluetoothAdapter mBluetoothAdapter;
     private ConnectThread mConnectThread;
+    private Button sendBtn;
+    private ConnectedThread dataSendingThread;
+    Handler mHandler;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        txtView = (TextView) findViewById(R.id.textView);
+//        txtView = (TextView) findViewById(R.id.textView);
         nReceiver = new NotificationReceiver();
         IntentFilter filter = new IntentFilter();
         filter.addAction("com.example.wadee.nots.NOTIFICATION_LISTENER_EXAMPLE");
         registerReceiver(nReceiver,filter);
 
-        //handle bluetooth
-        mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
-        if (mBluetoothAdapter == null) {
-        // Device does not support Bluetooth
-            Toast.makeText(this, "Sorry, Your Wretched device does not support Bluetooth", Toast.LENGTH_LONG).show();
-        }
-        //check if the bluetoooth is not enabled and show some UI to open Bluetooth
-        if (!mBluetoothAdapter.isEnabled()) {
-            Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
-            startActivityForResult(enableBtIntent, 1);
-        }
+        this.initiateBluetoothConnection();
 
         Set<BluetoothDevice> pairedDevices = mBluetoothAdapter.getBondedDevices();
+        BluetoothDevice mDevice = null;
         if (pairedDevices.size() > 0) {
             for (BluetoothDevice device : pairedDevices) {
-                mDevice = device;
+                if(device.getName().toString().equals("Wadee’s MacBook Pro")){
+                    Log.i(TAG, device.getName().toString());
+                    mDevice = device;
+                }
             }
         }
 
-        //start the connection thread
+
+        //initialize the connection thread
         mConnectThread = new ConnectThread(mDevice);
         mConnectThread.start();
 
-        Handler mHandler = new Handler() {
+        ///
+        sendBtn = (Button)findViewById(R.id.sendTextBtn);
+        sendBtn.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void handleMessage(Message msg) {
-                byte[] writeBuf = (byte[]) msg.obj;
-                int begin = (int)msg.arg1;
-                int end = (int)msg.arg2;
-
-                switch(msg.what) {
-                    case 1:
-                        String writeMessage = new String(writeBuf);
-                        writeMessage = writeMessage.substring(begin, end);
-                        break;
-                }
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Sending with Bt", Toast.LENGTH_LONG).show();
+                Log.i(TAG, "Send !!!");
+                MainActivity.this.dataSendingThread.write("bhjjbh".getBytes());
             }
-        };
+        });
+
     }
 
     @Override
@@ -78,11 +80,7 @@ public class MainActivity extends Activity {
 
 
 
-    public void buttonClicked(View v){
 
-
-
-    }
 
     class NotificationReceiver extends BroadcastReceiver{
 
@@ -90,7 +88,7 @@ public class MainActivity extends Activity {
         public void onReceive(Context context, Intent intent) {
             String temp = intent.getStringExtra("notification_event") + "\n" ;//+ txtView.getText();
             //send data here to arduino
-            txtView.setText(temp);
+//            txtView.setText(temp);
         }
     }
 
@@ -98,8 +96,8 @@ public class MainActivity extends Activity {
     private class ConnectThread extends Thread {
         private final BluetoothSocket mmSocket;
         private final BluetoothDevice mmDevice;
-        private static final UUID MY_UUID = UUID.fromString("00001101-0000-1000-8000-
-        00805f9b34fb");
+        private  final UUID MY_UUID = UUID.fromString("00001101-0000-1000-8000-00805f9b34fb");
+
         public ConnectThread(BluetoothDevice device) {
             BluetoothSocket tmp = null;
             mmDevice = device;
@@ -112,12 +110,18 @@ public class MainActivity extends Activity {
             mBluetoothAdapter.cancelDiscovery();
             try {
                 mmSocket.connect();
+                MainActivity.this.dataSendingThread = new ConnectedThread(mmSocket);
+                Log.i(TAG, "Connection thread working");
             } catch (IOException connectException) {
                 try {
-                    mSocket.close();
+                    mmSocket.close();
                 } catch (IOException closeException) { }
                 return;
             }
+
+
+//            dataSendingThread.start();
+
         }
         public void cancel() {
             try {
@@ -143,30 +147,36 @@ public class MainActivity extends Activity {
             mmOutStream = tmpOut;
         }
         public void run() {
-            byte[] buffer = new byte[1024];
-            int begin = 0;
-            int bytes = 0;
-            while (true) {
-                try {
-                    bytes += mmInStream.read(buffer, bytes, buffer.length - bytes);
-                    for(int i = begin; i < bytes; i++) {
-                        if(buffer[i] == "#".getBytes()[0]) {
-                            mHandler.obtainMessage(1, begin, i, buffer).sendToTarget();
-                            begin = i + 1;
-                            if(i == bytes - 1) {
-                                bytes = 0;
-                                begin = 0;
-                            }
-                        }
-                    }
-                } catch (IOException e) {
-                    break;
-                }
-            }
+
+            //send data using bluetooth
+            this.write("Hello".getBytes());
+            Log.i(TAG, "Send !!! from the worker thread");
+//            byte[] buffer = new byte[1024];
+//            int begin = 0;
+//            int bytes = 0;
+//            while (true) {
+//                try {
+//                    bytes += mmInStream.read(buffer, bytes, buffer.length - bytes);
+//                    for(int i = begin; i < bytes; i++) {
+//                        if(buffer[i] == "#".getBytes()[0]) {
+//                            mHandler.obtainMessage(1, begin, i, buffer).sendToTarget();
+//                            begin = i + 1;
+//                            if(i == bytes - 1) {
+//                                bytes = 0;
+//                                begin = 0;
+//                            }
+//                        }
+//                    }
+//                } catch (IOException e) {
+//                    break;
+//                }
+//            }
         }
         public void write(byte[] bytes) {
             try {
                 mmOutStream.write(bytes);
+                Log.i(TAG, "Inside the wriee method");
+
             } catch (IOException e) { }
         }
         public void cancel() {
@@ -176,4 +186,21 @@ public class MainActivity extends Activity {
         }
     }
 
+    private void sendCommandUsingBluetooth(String data){
+        this.mConnectThread.start();
+    }
+
+    private void initiateBluetoothConnection(){
+        //handle bluetooth
+        mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+        if (mBluetoothAdapter == null) {
+            // Device does not support Bluetooth
+            Toast.makeText(this, "Sorry, Your Wretched device does not support Bluetooth", Toast.LENGTH_LONG).show();
+        }
+        //check if the bluetooth is not enabled and show some UI to open Bluetooth
+        if (!mBluetoothAdapter.isEnabled()) {
+            Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivityForResult(enableBtIntent, 1);
+        }
+    }
 }
---
+++ app/src/main/java/com/example/wadee/nots/WriteWordActivity.java
@@ -0,0 +1,14 @@
+package com.example.wadee.nots;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+public class WriteWordActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_write_word);
+    }
+
+}
